42f09bbf7172885e3aa37af67d7ccb962b7166bd
Jonny Andreola <jonnyandreola@gmail.com>
2016-02-15 19:18:18 -0500

    Key concepts have been implemented, redux reducers doesnt seem a good idea

--- src/js/app.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Hello from './hello';
-const container = document.getElementById('app-container');
-
-ReactDOM.render(<Hello name='World' />, container);
